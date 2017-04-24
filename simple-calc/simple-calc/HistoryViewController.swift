adc11fe9425a8b6bed633f35a122f2eec101850d
Sabrina Niklaus <sabrinamarlo@Sabrinas-MBP.hsd1.wa.comcast.net>
2017-04-24 06:37:13 -0700

    Scrolling works

--- simple-calc/simple-calc/HistoryViewController.swift
+++ simple-calc/simple-calc/HistoryViewController.swift
@@ -11,18 +11,26 @@ import UIKit
 class HistoryViewController: UIViewController {
 
     public var history = [String]()
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var historyLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(history)
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height + 100)
+        let offset: Int = 30
+        var i = 0
         for item in history {
-            let newLabel = UILabel(frame: CGRect(x: 0, y: 75 + history.index(of: item)! * 30, width: 200, height: 21))
-            newLabel.textAlignment = .center
-            newLabel.text = item
-            self.view.addSubview(newLabel)
+            let label = UILabel(frame: CGRect(x: 0, y: i * offset, width: Int(self.view.frame.size.width), height: 25))
+            label.textColor = UIColor.black
+            label.textAlignment = .left
+            label.backgroundColor = UIColor.white
+            label.text = item
+            i += 1
+            self.scrollView?.addSubview(label)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
